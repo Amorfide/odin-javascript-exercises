e73efe88af105e7d2f5806966e8ce36f7c2d6ce7
amorfide <73080161+Amorfide@users.noreply.github.com>
2025-11-15 23:47:28 +0000

    created a string that is made up of the reversal of another string, using split, reverse and join methods

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let string = text.split("").reverse().join("")
+    return string;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
