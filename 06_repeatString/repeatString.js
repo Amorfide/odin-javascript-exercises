b722eae0f4ca2b7145787d4803e8ed9c011831ea
amorfide <73080161+Amorfide@users.noreply.github.com>
2025-11-15 23:27:36 +0000

    used math.random and return error for negative numbers, whilst trying to repeat a string a number of times

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(text, times) {
+    let repeatString = "";
+    if(times < 0){
+        return "ERROR";
+    }
+    
+    for(let i=0;i<times;i++){
+        repeatString += text;
+    }
+    return repeatString;
 };
+
+times = Math.random;
 
 // Do not edit below this line
 module.exports = repeatString;
