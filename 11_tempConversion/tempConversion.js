a03de69f3488e198d85e4b9b479a78f8d8cd3b9c
amorfide <73080161+Amorfide@users.noreply.github.com>
2025-11-22 15:07:57 +0000

    used formula for celsius and fahrenheit and returned the result

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fh) {
+  let c = Math.round(((fh - 32) * 5/9)*10)/10; 
+    
+  return c;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cs) {
+  let fh = Math.round((32 + 9*(cs/5))*10)/10
+
+  return fh;
 };
 
 // Do not edit below this line
