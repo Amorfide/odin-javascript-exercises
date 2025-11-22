ac204ead7bd9f751aa88918ec2b16d361e25582c
amorfide <73080161+Amorfide@users.noreply.github.com>
2025-11-22 15:43:57 +0000

    add, subtract, pwoer and factorial pretty straight forward, had to do quick research for arrays to iterate and apply operations

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for(i=0; i<array.length; i++){
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiplication = 1;
+  for(let i=0; i<array.length; i++){
+    multiplication *= array[i];
+  }
+  return multiplication;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let sum = 1;
+  if(a ==0){
+    return sum;
+  }else{
+    for(let i=1; i<=a; i++){
+      sum *= i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
