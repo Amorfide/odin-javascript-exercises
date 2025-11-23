ec4de7326b2474d6d4c06bfa327148c22d1c080e
amorfide <73080161+Amorfide@users.noreply.github.com>
2025-11-23 18:02:08 +0000

    created fib sequence and output any term

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(term) {
+    let termOne = 0
+    let termTwo = 1
+    let fibo = 0;
 
+    const sequence = [0]
+
+    for(let i = 0; i < term; i++){
+        sequence.push(termTwo);
+        fibo = termOne + termTwo;
+        termOne = termTwo;
+        termTwo = fibo;
+    }
+    return sequence[term];
+    
 };
 
 // Do not edit below this line
