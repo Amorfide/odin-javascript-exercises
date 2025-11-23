104aadd4a6625c3fdcb1230ee077d7636b1e44ae
amorfide <73080161+Amorfide@users.noreply.github.com>
2025-11-23 17:40:09 +0000

    replaced word with lowercase and removed punctuation, and put into an array, reversing it and joining into a new string

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    let array = word.split("");
+    let reversedWord = array.reverse().join("");
+    if(reversedWord == word){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
